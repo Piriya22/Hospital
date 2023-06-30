d487faab30ea4072f319a6bd2419e05ee73661b9
Piriya22 <127122879+Piriya22@users.noreply.github.com>
2023-06-30 18:31:24 +0530

    Commit1

---
+++ Back End/Hospital_BB/Hospital_BB/Repository/IDoctorsRepo.cs
@@ -0,0 +1,19 @@
+﻿using Hospital_BB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+
+namespace Hospital_BB.Repository
+{
+    public interface IDoctorsRepo
+    {
+        public IEnumerable<Doctors> GetDoctor();
+
+        public Doctors DoctorById(int Doctor_Id);
+        Task<Doctors> CreateDoctor([FromForm] Doctors doctors, IFormFile imageFile);
+        Task<Doctors> UpdateDoctor(int doctorid, Doctors doctor, IFormFile imageFile);
+        public Doctors DeleteDoctor(int doctorid);
+
+
+
+    }
+}
